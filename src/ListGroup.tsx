45560a22e58cc2b4a46be2aafb78d3791bcfc40f
Elrom5 <elrombenami12@gmail.com>
2025-01-12 17:55:38 +0200

    adding props

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,12 +1,50 @@
-function ListGroup() {
-    const items = ['An item', 'A second item', 'A third item', 'A fourth item', 'A fifth item']
+import {useState, FC} from 'react';
+
+interface ListGroupProps {  
+    title:string,
+    items:string[]
+
+}
+const ListGroup: FC<ListGroupProps> = ({title,items})=> {
+    const[selectedIndex, setSelectedIndex] = useState(0);
+   /*const [items] = useState([
+        'An item', 'A second item', 'A third item', 'A fourth item', 'A fifth item'])*/
+
+    const [rendet, setRender] = useState(0)
+
+    console.log('ListGroup render')
+
+    const onClick = (index: number) => {
+        console.log('click ' + index)
+        setSelectedIndex (index)
+    }
+
+    const addItem = () => 
+        {
+        console.log('add item')
+        items.push("newItem")
+        setRender(rendet + 1)
+        
+         }
+
     return (
-        <ul className = "list-group">
+        <div>
+            <h1>{title}</h1>
+            {items.length === 0 ? <p>No items</p> :
+            <ul className = "list-group">
             {items.map((item, index) => {
-                return <li className="list-group-item">{index} {item}</li>
-            })}
-        </ul>
+                return <li 
+                className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                key={index} 
+                onClick ={() => {onClick(index) }}>
+                {index} {item}
+                 </li>
+             })}
+             </ul>
+             }
+             <button onClick = {() => {addItem()}}>Add item</button>
+        </div>
     )
 }
 
-export default ListGroup;
+export default ListGroup; 
